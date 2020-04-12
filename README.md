7a113ef50939c375099f6c7f4aa32a7eb36fbd50
Alf Stenbrunn <5901432+redReno@users.noreply.github.com>
2020-04-12 14:57:09 +0100

    Separate led control from thermometer
    Run the program using '... 0 1 &'
    to start a background process that turns a led on and off.
    To control a led, use kill -SIGINT <PID>
    To stop, use kill -SIGKILL <PID>

--- README.md
+++ README.md
@@ -4,5 +4,15 @@ The temperature is in Celcius, with two digits.
 
 # To run
 cd temperature_sensor_c
-gcc -o rpi-temp.out thermostat.c 
+gcc -Wall -pthread -o rpi_temp.out thermostat.c -lpigpio -lrt
+sudo chown root:root rpi_temp.out
+sudo chmod 4755 rpi_temp.out
+
 ./rpi-temp.out (this runs the program once, and can be repeated)
+
+There are two possible parameters (sequential):
+debug (1 or 0) 
+  - 1 if you want explanatory logs. Default 0.
+led_controller (1 or 0)
+  - 1 if you want to control a LED using SIGINT calls. Default 0.
+    The led should get its power from GPIO14.
--- temperature_sensor_c/thermostat.c
+++ temperature_sensor_c/thermostat.c
@@ -9,22 +9,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pigpio.h>
+#include <signal.h>
 
 #define BASE_DIR "/sys/bus/w1/devices/"
 #define PIN_ADDRESS "28-00000adb99e5"
 #define DEVICE_FILE BASE_DIR PIN_ADDRESS "/w1_slave"
 #define NUMBER_OF_DECIMALS 3
 
-void run_thermostat(int debug);
+void run_thermostat(int);
 void extract_temp(char *, char *);
+void run_led_controller(void);
+void handle_led_request(int);
+void handle_sigterm();
 
 int main(int argc, char **argv)
 {
   int debug = 0;
+  int led_controller = 0;
+
   if (argc > 1) {
     debug = atoi(argv[1]); 
   }
+  if (argc > 2) {
+    led_controller = atoi(argv[2]);
+    if (led_controller)
+    {
+      run_led_controller();
+    }
+  }
   run_thermostat(debug);
+  exit(EXIT_SUCCESS);
+}
+
+void run_led_controller() {
+  if (gpioInitialise() < 0)
+  {
+    printf("fail");
+    exit(EXIT_FAILURE);
+  }
+  gpioSetMode(14, PI_OUTPUT);
+ // create signal handlers
+  signal(SIGINT, handle_led_request);
+  signal(SIGTERM, handle_sigterm);
+  // run until SIGTERM
+  while (1)
+  {
+    ;
+  } 
+}
+
+void handle_led_request(int sig) {
+  gpioWrite(14, 1);
+  time_sleep(0.5);
+  gpioWrite(14, 0);
+  // reset signal
+  signal(SIGINT, handle_led_request);
+}
+
+void handle_sigterm() {
+  // stop all led handling
+  signal(SIGINT, SIG_DFL);
+  gpioWrite(14, 0);
+  gpioTerminate();
   exit(EXIT_SUCCESS);
 }
 
